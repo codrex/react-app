7e0248b2c058deeacf6755fe4e4c136773862233
Rex Ogbemudia <ogbemudiarex@gmail.com>
2018-06-06 04:42:05 +0100

    feat(caption) create caption area

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Button from './components/Button';
+import CaptionArea from './components/CaptionArea';
 
 import './styles.scss';
 
@@ -9,10 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Button className="btn--red" text="Facebook" />
-        <Button className="btn--red" text="Instagram" />
-        <Button className="btn--red btn--arrow" withoutText />
-        <Button className="btn--white btn--arrow" text="read more" />
+        <CaptionArea />
       </React.Fragment>
     );
   }
